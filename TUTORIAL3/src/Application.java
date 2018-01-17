42f88c8097299e6c1d2d1e89f65f6b892e8053b8
Casey Zimmerman <caseyezimmerman@gmail.com>
2018-01-17 12:49:39 -0500

    java practice

---
+++ TUTORIAL3/src/Application.java
@@ -0,0 +1,18 @@
+
+public class Application {
+	public static void main(String[] args) {
+		int myInt = 7;
+		
+		/////string is a class
+		String text = "Hello";
+		String blank = " ";
+		String name = "Bob";
+		String greeting = text + blank + name;
+			
+		System.out.println(greeting);
+		System.out.println("hello" + " " + "Bob");
+		
+		double myDouble = 7.8;
+		System.out.println("My number is: " + myDouble + ".");
+	}
+}
